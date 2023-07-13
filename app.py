6c891a2954c440d35e54f753d2dff0b44e39351b
nazmus.saqib <nazmus.saqib@optofidelity.com>
2023-07-13 14:21:26 +0300

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS=[
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bengluru",
+        "Salary": 100000
+    },
+{
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Delhi",
+        "Salary": 150000
+    },
+{
+        "id": 3,
+        "title": "Frontend Engineer",
+        "location": "Remote",
+    },
+{
+        "id": 4,
+        "title": "Backend Engineer",
+        "location": "San Fransisco",
+        "Salary": 120000
+    }
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
